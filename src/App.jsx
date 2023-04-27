e14ab97c18a245aa538c3c7cd01f8c5a2c85bedd
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-27 11:22:27 +0300

    useCallback useMemo testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
+import React, {useCallback, useMemo, useState} from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  return <div className="App">
+    <RandomColorButtonsPanel/>
+  </div>;
 }
 
-export default App
+const RandomColorButton = React.memo(({color, onClick}) => {
+  return (
+      <button
+        onClick={onClick}
+        style={{
+          width: '50px',
+          height: '50px',
+          background: color,
+          margin: '10px'
+        }}
+      />
+  );
+})
+
+function RandomColorButtonsPanel() {
+  const colors = useMemo(() => ['yellow', 'blue', 'green', 'red', 'pink', 'purple'], []);
+  const getRandomColor = useCallback(() => colors[Math.round(Math.random() * (colors.length - 1))], [colors]);
+  const [randomColor1, setRandomColor1] = useState(colors[0]);
+  const [randomColor2, setRandomColor2] = useState(colors[0]);
+
+  const changeRandomColor1 = useCallback(() => setRandomColor1(getRandomColor()), [getRandomColor]);
+  const changeRandomColor2 = useCallback(() => setRandomColor2(getRandomColor()), [getRandomColor]);
+
+  return <div>
+    <RandomColorButton color={randomColor1} onClick={changeRandomColor1}/>
+    <RandomColorButton color={randomColor2} onClick={changeRandomColor2}/>
+  </div>
+}
